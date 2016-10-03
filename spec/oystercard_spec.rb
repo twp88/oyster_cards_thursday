99e1484e3630ad5becdf0a1ed67f65d78918bf8f
Anna Beddow <anna.beddow@gmail.com>
2016-10-03 17:21:41 +0100

    refactored oystercard tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,14 +3,6 @@ require 'oystercard'
 describe OysterCard do
   subject(:card) {described_class.new}
 
-  it 'takes a balance'do
-  expect(card).to respond_to(:balance)
-  end
-
-  it 'can be topped up' do
-    expect(card).to respond_to(:top_up).with(1).argument
-  end
-
   it 'adds top-up to balance'do
     card.top_up(40)
     expect(card.balance).to eq 40
