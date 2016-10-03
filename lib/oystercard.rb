38e6ab9a0d8929b572d63ae2033193a3746a9a32
jinimcoroneo <jinim1983@gmail.com>
2016-10-03 17:56:45 +0100

    exercise 6, adding a constant for maximum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,15 @@
 class OysterCard
-attr_reader :balance
+  MAX_LIMIT = 90
+  BALANCE = 0
+attr_reader :balance, :max_limit
 
-  def initialize(balance=0)
+  def initialize(max_limit = MAX_LIMIT, balance = BALANCE)
+    @max_limit = max_limit
     @balance = balance
   end
 
   def top_up(value)
+    raise "balance cannot exceed #{@max_limit} pounds" if @balance + value >= 90
     @balance += value
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,9 +3,13 @@ require 'oystercard'
 describe OysterCard do
   subject(:card) {described_class.new}
 
-  it 'adds top-up to balance'do
-    card.top_up(40)
-    expect(card.balance).to eq 40
+  it 'adds top-up to balance' do
+    card.top_up(0)
+    expect(card.balance).to eq 0
   end
 
+  it 'raises an error if balance is more than 90' do
+    card.top_up(90)
+    expect {card.top_up(5)}.to raise_error "balance cannot exceed 90 pounds"
+  end
 end
