e891c6abd8ca3215de23b741a0094d8aec43471a
Anna Beddow <anna.beddow@gmail.com>
2016-10-03 17:08:36 +0100

    complete 04 - adding balance attribute to card

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,10 @@
-describe do
+require 'oystercard'
+
+describe OysterCard do
+  subject(:card) {described_class.new}
+
+  it 'takes a balance'do
+  expect(card).to respond_to(:balance)
+  end
 
 end
