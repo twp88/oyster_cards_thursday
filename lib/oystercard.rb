5b4ed02562597bcbe6fb6c0fcfe1fc237f6d22cf
Anna Beddow <anna.beddow@gmail.com>
2016-10-03 17:17:06 +0100

    added top_up method to add money to balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,7 +3,10 @@ attr_reader :balance
 
   def initialize(balance=0)
     @balance = balance
+  end
 
+  def top_up(value)
+    @balance += value
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,4 +7,13 @@ describe OysterCard do
   expect(card).to respond_to(:balance)
   end
 
+  it 'can be topped up' do
+    expect(card).to respond_to(:top_up).with(1).argument
+  end
+
+  it 'adds top-up to balance'do
+    card.top_up(40)
+    expect(card.balance).to eq 40
+  end
+
 end
